252e6e1160020d82fa16e061cc597ca43372eb1a
surya <surya@gmail.com>
2023-11-06 06:53:52 +0000

    3rd 2 files added

---
+++ five.java
@@ -0,0 +1,8 @@
+class five
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, from five");
+    }
+}
+
---
+++ six.java
@@ -0,0 +1,7 @@
+class six
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, from six");
+    }
+}
