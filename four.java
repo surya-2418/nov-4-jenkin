40770eba033c8df9bf9325ddd43bc258dd28548e
surya <surya@gmail.com>
2023-11-06 06:48:13 +0000

    2nd 2 files added

---
+++ four.java
@@ -0,0 +1,7 @@
+class four
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, from four")
+    }
+}
---
+++ three.java
@@ -0,0 +1,7 @@
+class three
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, from three");
+    }
+}
